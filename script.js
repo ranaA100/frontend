60d327cd4517b08493d1e97d20ff64814ea4dd19
antu <anturana092@gmail.com>
2024-07-17 19:32:35 +0530

    interactive landing page created

---
+++ script.js
@@ -0,0 +1,4 @@
+ document.getElementById('signup-btn').addEventListener('click', function () {
+                alert('Thank you for signing up!');
+                // You can add more functionality here such as opening a signup form, etc.
+            });
